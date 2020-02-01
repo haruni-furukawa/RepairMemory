3bd1ae8a485ac4606d497114a985c76be4f2d5e8
noshita <>
2020-02-01 10:24:20 +0900

    [fix] インゲームスクリプト仮配置

---
+++ RepairMemory/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Player player;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            player.runUp();
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            player.runDown();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player.runRight();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.runLeft();
+        }
+
+    }
+}
---
+++ RepairMemory/Assets/Scripts/View/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void runUp()
+    {
+        Debug.Log("runUp");
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    public void runDown()
+    {
+        transform.position -= transform.forward * speed * Time.deltaTime;
+    }
+    public void runRight()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+    public void runLeft()
+    {
+        transform.position -= transform.right * speed * Time.deltaTime;
+    }
+
+
+    //if (bKey)
+    //{
+    //    this.animator.SetBool(isRunning, true);
+    //}
+    //else
+    //{
+    //    this.animator.SetBool(isRunning, false);
+    //}
+}
