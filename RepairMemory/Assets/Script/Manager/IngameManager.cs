61e27413f6587e884d1882f0f0dc96651c679ec7
noshita <>
2020-02-01 12:49:03 +0900

    [refactor] フォルダ名変更

---
+++ RepairMemory/Assets/Script/Manager/IngameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameManager : MonoBehaviour
+{
+    public void CreateEnemies(string prefabName, int count, float x, float y)
+    {
+        var prefab = (GameObject)Resources.Load("Prefabs/" + prefabName);
+        var objEnemy = Instantiate(prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+
+        //cardView = objCard.GetComponent(typeof(FollowerCardView)) as FollowerCardView;
+        //Instantiate(p)
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
