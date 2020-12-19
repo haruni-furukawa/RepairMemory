fc94c9764bbc898d91679352b5246b04ec92207d
ryo <>
2020-12-20 01:12:04 +0900

    Resource位置を修正

--- RepairMemory/Assets/Scripts/UI/Const/ResourceConst.cs
+++ RepairMemory/Assets/Scripts/UI/Const/ResourceConst.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public static class ResourceConst
 {
-    public static readonly string JSON_PATH = "Data/"; //json格納ディレクトリ
+    public const string JSON_PATH = "Data/"; // json格納ディレクトリ
+    public const string ENEMIES_PREFABS_PATH = "Prefabs/Enemies/"; // エネミープレハブディレクトリ
+    public const string EFFECTS_PREFABS_PATH = "Prefabs/Effects/"; // エフェクトプレハブディレクトリ
+    public const string STAND_TEXTURES_PATH = "Textures/Character/Stand/"; // 立ち絵ディレクトリ
 }
