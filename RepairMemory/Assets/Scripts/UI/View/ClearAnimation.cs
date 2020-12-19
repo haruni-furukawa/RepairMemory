97f6dd039adb363beea42e29d86aa586213ecbbc
ryo <>
2020-12-20 02:28:11 +0900

    クリア・ゲームーオーバーアニメーションをDOTween処理に置換

--- RepairMemory/Assets/Scripts/UI/View/ClearAnimation.cs
+++ RepairMemory/Assets/Scripts/UI/View/ClearAnimation.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+
 public class ClearAnimation : MonoBehaviour
 {
+    private bool _isPlaying = false;
+    private bool _isProcessed = false;
+
+    void Start() { Play(); }
     public void Play()
     {
-        GetComponent<Animator>().SetTrigger("Play");
+        if (_isPlaying || _isProcessed) { return; }
+
+        _isPlaying = true;
+        float duration = 1.0f / 6.0f;
+        float delayTime = 1.0f;
+        Vector3 initializeScale = Vector3.one;
+        initializeScale.y = 0.0f;
+        transform.transform.localScale = initializeScale;
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOScaleY(1.0f, duration).SetLink(gameObject));
+        seq.AppendInterval(delayTime);
+        seq.Append(transform.DOScaleY(0.0f, duration).SetLink(gameObject));
+        seq.AppendCallback(() => OnEndAnimation());
     }
-    public void OnEndAnimation()
+
+    private void OnEndAnimation()
     {
+        _isPlaying = false;
+        _isProcessed = true;
         SoundManager.Instance?.StopAllBgm();
         SceneManager.LoadScene("Clear");
     }
--- RepairMemory/Assets/Scripts/UI/View/GameOverAnimation.cs
+++ RepairMemory/Assets/Scripts/UI/View/GameOverAnimation.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+
 public class GameOverAnimation : MonoBehaviour
 {
+    private bool _isPlaying = false;
+    private bool _isProcessed = false;
     public void Play()
     {
-        GetComponent<Animator>().SetTrigger("Play");
+        if (_isPlaying || _isProcessed) { return; }
+
+        _isPlaying = true;
+        float duration = 1.0f / 6.0f;
+        float delayTime = 1.0f;
+        Vector3 initializeScale = Vector3.one;
+        initializeScale.y = 0.0f;
+        transform.transform.localScale = initializeScale;
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOScaleY(1.0f, duration).SetLink(gameObject));
+        seq.AppendInterval(delayTime);
+        seq.Append(transform.DOScaleY(0.0f, duration).SetLink(gameObject));
+        seq.AppendCallback(() => OnEndAnimation());
     }
-    public void OnEndAnimation()
+    private void OnEndAnimation()
     {
+        _isPlaying = false;
+        _isProcessed = true;
         SoundManager.Instance?.StopAllBgm();
         SceneManager.LoadScene("Credit");
     }
